e8c64a5b3b17a3749b672755323427463c21ec2c
Nicholas Clarkson <nicholasclarkson@Nicholass-MacBook-Pro.local>
2020-10-02 15:38:29 +0100

    twig refactor

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 sass --watch assets/scss/main.scss:assets/css/main.css
+
+https://stark-temple-23774.herokuapp.com/
--- index.php
+++ index.php
@@ -6,94 +6,44 @@
     use Symfony\Component\Yaml\Yaml;
 
     $data = Yaml::parseFile('cv.yml');
+    $data['last_updated'] = $last_updated;
+    $data['cache_buster'] = $cache_buster;
 
-    $cache_buster = filemtime('assets/css/main.css');
-    $last_updated = filemtime('cv.yml');
+    function format($dt){
 
-    // echo $cache_buster;
-    // echo $last_updated;
+    }
+
+    ////////////
+
+    $loader = new \Twig\Loader\FilesystemLoader('templates');
+
+    $twig = new \Twig\Environment($loader, [
+        'cache' => false // 'templates_cache',
+    ]);
+
+
+    // filters
+
+    $filter = new \Twig\TwigFilter('format_date', function ($dt) {
+        $o = "Current";
+        if($dt != 'Current'){
+            $o = gmdate("M, Y", $dt); 
+        }
+        return $o;
+    });
+
+    $twig->addFilter($filter);
+
+    //////////
+
+    $filter = new \Twig\TwigFilter('markdown', function ($string) {
+        return Markdown::defaultTransform($string);
+    });
+
+    $twig->addFilter($filter);
+
+    // render
+
+    echo $twig->render('lowfi.twig', $data);
 
 ?>
-<!DOCTYPE html>
-<html lang="en-GB" class="html ">
-    <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title><?=$data["name"]?>, <?=$data["title"]?>, <?=$data["location"]?></title>
-        <meta name="description" content="<?=$data["title"]?>">
-        <meta name="generator" content="ZX Spectrum 128 +2" />
-        <meta name="author" content="<?=$data["name"]?>">
-        <meta name="twitter:card" content="summary_large_image">
-        <meta name="twitter:site" content="@t_pk" />
-        <meta name="twitter:creator" content="@t_pk" />
-        <link rel="icon" type="image/png" href="" /> <!-- @todo -->
-
-        <link rel="stylesheet" href="/assets/css/main.css?<?=$cache_buster?>">
-
-        <meta property="og:image" content=""/>  <!-- @todo -->
-        <meta property="og:title" content="<?=$data["name"]?>"/>
-        <meta property="og:description" content="<?=$data["title"]?>"/>
-        <meta property="og:site_name" content="<?=$data["name"]?>"/>
-        <meta property="og:url" content="https://clar.ky"/>
-        <meta property="og:type" content="website" />
-        <meta property="fb:app_id" content="1592717320815147" /> 
-    </head>
-
-    <body>
-
-        <div class="page">
-
-            <header>
-                <h1 class="u-no-gutter"><?=$data["name"]?></h1>
-                <h2 class="u-no-gutter"><?=$data["title"]?></h2>
-                <h3><?=$data["location"]?></h3>
-            </header>
-
-            <section>
-                <?=Markdown::defaultTransform($data["personal_statement"])?>    
-            </section>
-            
-            <section>
-                <h2>Social</h2>
-                <ul>
-                    <li><a href="<?=$data['contact']['twitter']?>">Twitter</a></li>
-                    <li><a href="<?=$data['contact']['instagram']?>">Instagram</a></li>
-                    <li><a href="<?=$data['contact']['strava']?>">Strava</a></li>
-                </ul>
-            </section>
-
-            <h2>Work experience</h2>
-            
-            <?php foreach($data["work_experience"] as $job) : ?>    
-                <section>     
-                    <h3 class="u-no-gutter"><?=$job["company"]?></h3>
-                    <h4 class="u-no-gutter"><?=$job["position"]?></h4>
-                    <p><em><?=$job["start"]?> - <?=$job["end"]?></em></p>
-                    <?=Markdown::defaultTransform($job["description"])?>         
-                </section>
-            <?php endforeach ?>
-
-            <section>
-                <h2>Skills</h2>
-                <div class="v2g">
-                    <?php foreach($data["skills"] as $key => $cloud) : ?> 
-                        <div class="skill-set v2g__box v2g__box--sml-haf v2g__box--lrg-thd">                        
-                            <h3><?=$key?></h3>
-                            <ul>
-                                <?php foreach($cloud as $skill) : ?>    
-                                    <li><?=$skill?></li>
-                                <?php endforeach ?>
-                            </ul>                    
-                        </div>
-                    <?php endforeach ?>
-                </div>
-            </section>
-            
-            <footer>
-                <?=$last_updated?><br />
-                https://stark-temple-23774.herokuapp.com/
-            </footer>
-
-        </div>
-
-    </body>
-</html>
