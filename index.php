03276dba9754924d8a66712beea901ba8d8c39ab
Nicholas Clarkson <nicholasclarkson@Nicholass-MacBook-Pro.local>
2020-10-04 16:19:25 +0100

    redirect in php

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php 
     
-    if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+    // if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
         $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-        header('HTTP/1.1 301 Moved Permanently');
-        header('Location: ' . $location);
+        // header('HTTP/1.1 301 Moved Permanently');
+        // header('Location: ' . $location);
+
+        echo $location;
+
         exit;
-    }
+    // }
 
     require __DIR__ . '/vendor/autoload.php';
 
