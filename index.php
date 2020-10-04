42db32c1e5cf4996985cea326ce9f6bec5ec56d6
Nicholas Clarkson <nicholasclarkson@Nicholass-MacBook-Pro.local>
2020-10-04 16:17:05 +0100

    redirect in php

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php 
     
+    if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+        $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+        header('HTTP/1.1 301 Moved Permanently');
+        header('Location: ' . $location);
+        exit;
+    }
+
     require __DIR__ . '/vendor/autoload.php';
 
     use Michelf\Markdown;
